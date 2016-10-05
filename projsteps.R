de7e0a774aa93939fc63e2bd7e4f60519fe6d4cb
Conrad De Peuter <conrad.depeuter@gmail.com>
2016-10-05 18:38:04 -0400

    start of proj

---
+++ projsteps.R
@@ -0,0 +1,18 @@
+#get gene omnibus
+
+#do regular gene expression analysis (which ones is impt)
+
+#get distance matrix
+
+#do mapper, what clusters do the important genes end up in
+ 
+#if multiple, whys that
+
+#regardless of multiple or single, what genes were not significantly expressed
+# but are in the same cluster as the significantly expressed ones
+
+
+#look at the pathways of the important genes
+#can we make any insight from those
+
+#use TOPAseq for some topological analysis of the pathways
