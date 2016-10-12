5d9ddfa2d1a9ec6ca4c636847b74625983e743cf
vijaybalaji30 <vijaybalaji30@gmail.com>
2016-10-11 22:13:41 -0400

    Update projSteps2.R

--- projSteps2.R
+++ projSteps2.R
@@ -155,3 +155,37 @@ getProp_reg <- function(clusternumber){length(which(which(toClusterReg==clustern
 pct_diffexp_reg  <- unlist(lapply(seq(from = 1, to = m1$num_vertices), getProp_reg))
 plotcluster(fil3,regClust$cluster)
 clusplot(fil3,regClust$cluster,color=TRUE,shade=TRUE,lines=0)
+
+clusters=list()
+for(i in 1:m1$num_vertices){
+  clusters[[i]]=which(toCluster %in% i)
+}
+#x=matrix(nrow=2,ncol=2)
+#fisher=list()
+#min=1
+#for(i in 1:length(pct_diffexp)){
+  #x[1,1]=length(which(m1$points_in_vertex[[i]] %in% diff_gene_nums))
+  #print(x[1,1])
+  #x[1,2]=length(diff_genes)-x[1,1]
+  #print(x[1,2])
+  #x[2,1]=length(m1$points_in_vertex[[i]])-x[1,1]
+  #print(x[2,1])
+  #x[2,2]=numgenes-length(diff_genes)-x[2,1]
+  #print(x[2,2])
+  #fisher[[i]]=fisher.test(x,alternative = "greater")
+  #if(min>fisher[[i]]$p.value){min=fisher[[i]]$p.value}
+#}
+#fisherreg=list()
+#minreg=1
+#for(i in 1:length(pct_diffexp)){
+  #x[1,1]=length(which(which(toClusterReg==i) %in% diff_gene_nums ))
+  #print(x[1,1])
+  #x[1,2]=length(diff_genes)-x[1,1]
+  #print(x[1,2])
+  #x[2,1]=length(which(toClusterReg==i))-x[1,1]
+  #print(x[2,1])
+  #x[2,2]=numgenes-length(diff_genes)-x[2,1]
+  #print(x[2,2])
+  #fisherreg[[i]]=fisher.test(x,alternative = "greater")
+  #if(minreg>fisherreg[[i]]$p.value){minreg=fisherreg[[i]]$p.value}
+#}
