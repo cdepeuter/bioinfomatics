665db88a31d01a780a3c1111d41f5f30d91991f1
Conrad De Peuter <conrad.depeuter@gmail.com>
2016-12-02 15:46:47 -0500

    vijays changes

--- App-Directory/BHIcomparison.R
+++ App-Directory/BHIcomparison.R
@@ -3,17 +3,17 @@
 debug.print("BHI analysis")
 names(Genebelongstocluster)=names(unlist(allClustersGenes))
 
-m1.bhi = BHI(Genebelongstocluster, annotation="moe430a.db", names=names(Genebelongstocluster), category="all")
+m1.bhi = BHI(Genebelongstocluster, annotation=annotation_file, names=names(Genebelongstocluster), category="all")
 
-hclusters.bhi = BHI(hclusters.clusts,annotation="moe430a.db",names=names(hclusters.clusts),category = "all")
+hclusters.bhi = BHI(hclusters.clusts,annotation=annotation_file,names=names(hclusters.clusts),category = "all")
 
 
 m1.diffGenesInClust = Genebelongstocluster[which(names(Genebelongstocluster) %in% diff_genes)]
 hclusters.diffGenesInClusts = hclusters.clusts[which(names(hclusters.clusts) %in% diff_genes)]
 kclust.diffGenesInClusts = kclust$cluster[which(names(kclust$cluster) %in% diff_genes)]
-m1.bhiDiffGenes = BHI(m1.diffGenesInClust, annotation="moe430a.db", names=names(m1.diffGenesInClust), category="all")
-hclusters.bhiDiffGenes = BHI(hclusters.diffGenesInClusts,annotation="moe430a.db",names=names(hclusters.diffGenesInClusts),category="all")
-kclust.bhiDiffGenes = BHI(kclust.diffGenesInClusts,annotation="moe430a.db",names=names(kclust.diffGenesInClusts),category="all")
+m1.bhiDiffGenes = BHI(m1.diffGenesInClust, annotation=annotation_file, names=names(m1.diffGenesInClust), category="all")
+hclusters.bhiDiffGenes = BHI(hclusters.diffGenesInClusts,annotation=annotation_file,names=names(hclusters.diffGenesInClusts),category="all")
+kclust.bhiDiffGenes = BHI(kclust.diffGenesInClusts,annotation=annotation_file,names=names(kclust.diffGenesInClusts),category="all")
 
 
 #write to table
